f264cccb0cd8ebaabf37dceeb91fe958ca90fce6
sui <1202ayasnppk@gmail.com>
2024-08-03 17:39:44 +0900

    ci adn test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,19 @@
 /** @type {import('eslint').ESLint.ConfigData} */
 module.exports = {
   // prettierは他の設定の上書きを行うために、必ず最後に配置する。
+  env: {
+    'jest/globals': true,
+  },
   extends: [
     'next/core-web-vitals',
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@next/next/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jest/recommended', // jestプラグインを追加
     'prettier',
   ],
+  plugins: ['jest'], // プラグインにjestを追加
   rules: {
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
