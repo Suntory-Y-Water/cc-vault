373188eeeef37a3f1ab3c78747d29a0c4a99baaa
sui <1202ayasnppk@gmail.com>
2024-08-03 16:14:02 +0900

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 /** @type {import('eslint').ESLint.ConfigData} */
 module.exports = {
-  extends: ['next/core-web-vitals', 'prettier'],
+  // prettierは他の設定の上書きを行うために、必ず最後に配置する。
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@next/next/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
 };
